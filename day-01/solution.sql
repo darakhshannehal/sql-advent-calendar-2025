fbaa524af8b77b9e3df65bec0a012b487088994a
Darakhshan Nehal <darakhshannehal@gmail.com>
2025-12-01 21:14:09 +0530

    Add Day 1 SQL solution

---
+++ day-01/solution.sql
@@ -0,0 +1,9 @@
+-- Day 1: Reindeer Run Challenge
+-- Question:
+-- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh.
+-- Write a query to return the name and rank of the top 7 reindeers in this race.
+
+SELECT name, rank
+FROM reindeer_run_results
+ORDER BY rank ASC
+LIMIT 7;
