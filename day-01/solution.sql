6322e5e043bb3240e03c760d98e244122097a3b6
Darakhshan Nehal <darakhshannehal@gmail.com>
2025-12-04 22:06:06 +0530

    Tidy up

--- day-01/solution.sql
+++ day-01/solution.sql
@@ -1,7 +1,11 @@
 -- Day 1: Reindeer Run – Find the Top 7
+
 -- Question:
 -- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh.
 -- Write a query to return the name and rank of the top 7 reindeers in this race.
+
+-- Tables
+-- reindeer_run_results(number, name, rank, color)
 
 SELECT name, rank
 FROM reindeer_run_results
--- day-02/solution.sql
+++ day-02/solution.sql
@@ -1,4 +1,5 @@
 -- Day 2: Toys Delivered - Find Which Toys Made It
+
 -- Question:
 -- Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. 
 -- You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
