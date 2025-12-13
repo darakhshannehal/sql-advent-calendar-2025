e1669e7b3e3bc8118b9c4441b6e5fd20bdaf608f
Darakhshan Nehal <darakhshannehal@gmail.com>
2025-12-13 21:59:46 +0530

    tidy up

--- day-12/solution.sql
+++ day-12/solution.sql
@@ -1,7 +1,7 @@
 /*
 Day 12: North Pole Network Most Active Users
 
-Task:
+Question:
 The North Pole Network wants to see who's the most active in the holiday chat each day.
 Write a query to count how many messages each user sent, then find the most active user(s) each day.
 If multiple users tie for first place, return all of them.
